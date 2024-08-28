7259410caa120e50858c152d15b780dc95aaf8eb
Mark Malstrom <mark@malstrom.me>
2024-08-28 02:34:55 -0500

    Add vimeo-dl formula

---
+++ Formula/vimeo-dl.rb
@@ -0,0 +1,13 @@
+class VimeoDL < Formula
+    desc "A tool to download private videos hosted on Vimeo."
+    homepage "https://github.com/markmals/vimeo-dl"
+    url "https://github.com/markmals/vimeo-dl/archive/v0.1.0.tar.gz"
+    sha256 "01c625d07ba5c1a600778c8675d6ce0c28f925902446094df5d859334fbc0d96"
+
+    depends_on "deno" => :build
+
+    def install
+        system "deno", "run", "compile"
+        bin.install "vimeo-dl"
+    end
+end
