4a6650c88b7fadd32b5a26ef7749a2cb5289a578
Mark Malstrom <mark@malstrom.me>
2024-08-28 03:34:07 -0500

    Run brew style on the vimeo-dl formula

--- Formula/vimeo-dl.rb
+++ Formula/vimeo-dl.rb
@@ -1,17 +1,17 @@
 class VimeoDl < Formula
-    desc "A tool to download private videos hosted on Vimeo."
-    homepage "https://github.com/markmals/vimeo-dl"
-    url "https://github.com/markmals/vimeo-dl/archive/v0.1.0.tar.gz"
-    sha256 "01c625d07ba5c1a600778c8675d6ce0c28f925902446094df5d859334fbc0d96"
+  desc "Tool to download private videos hosted on Vimeo"
+  homepage "https://github.com/markmals/vimeo-dl"
+  url "https://github.com/markmals/vimeo-dl/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "01c625d07ba5c1a600778c8675d6ce0c28f925902446094df5d859334fbc0d96"
 
-    depends_on "deno" => :build
+  depends_on "deno" => :build
 
-    def install
-        system "deno", "task", "compile"
-        bin.install "vimeo-dl"
-    end
+  def install
+    system "deno", "task", "compile"
+    bin.install "vimeo-dl"
+  end
 
-    test do
-        assert_match "0.1.0", shell_output(bin/"vimeo-dl -V", 255)
-    end    
+  test do
+    assert_match "0.1.0", shell_output(bin/"vimeo-dl -V", 255)
+  end
 end
